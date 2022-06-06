20769ff0c74ba4472b7f4a58097e471d6a53c823
Michael Perry <moperrry@gmail.com>
2022-06-06 19:40:07 -0400

    displayed song elments

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let searchBox = document.getElementById("searchBox");
-
 let search = document.getElementById("search");
+const searchResults = document.getElementById("search-results");
 
 function buildSongs(itunesData) {
   console.log(itunesData);
@@ -25,8 +25,36 @@ search.addEventListener("click", (event) => {
     })
     .then(function (data) {
       buildSongs(data.results);
+      console.log(data.results);
       // if search has no results return message to user.
+
+      // loop artist album, name, art, and song name
+      for (let song of data.results) {
+        let songDiv = document.createElement("div");
+        songDiv.classList.add("song-info");
+        // Song Name
+        let songEl = document.createElement("song");
+        songEl.classList.add("song-name");
+        songEl.innerText = `"${song.trackName}"`;
+        songDiv.appendChild(songEl);
+        // Artist name
+        let artistDiv = document.createElement("div");
+        artistDiv.classList.add("artist");
+        artistDiv.innerText = song.artistName;
+        songDiv.appendChild(artistDiv);
+        console.log(song.artistName);
+        //Album name
+        let albumDiv = document.createElement("div");
+        albumDiv.classList.add("album");
+        albumDiv.innerText = song.collectionName;
+        songDiv.appendChild(albumDiv);
+        //Art
+        let imageEl = document.createElement("img");
+        imageEl.src = song.artworkUrl100;
+        imageEl.classList.add("art");
+        songDiv.appendChild(imageEl);
+
+        searchResults.appendChild(songDiv);
+      }
     });
 });
-
-// loop artist  album, name, album art, and track
